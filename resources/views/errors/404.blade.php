a66018d352321031a1c92bdb9689636ce95eda1b
MoffatMore <12894381+MoffatMore@users.noreply.github.com>
2020-04-13 20:56:33 +0200

    customixing error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
-@extends('errors::minimal')
+@extends('errors::custom-error')
 
-@section('title', __('Not Found'))
+@section('title', __('PAGE NOT FOUND'))
 @section('code', '404')
 @section('message', __('The page you are looking for might have been removed had its name changed or is temporarily unavailable.'))
